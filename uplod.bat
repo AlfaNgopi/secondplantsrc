8d75ede2ddbcb9343ca6ee9df244021a61e90fc2
Alfa <210711378@students.uajy.ac.id>
2025-01-14 09:06:22 +0700

    Tue-01-14 09-06

--- uplod.bat
+++ uplod.bat
@@ -1,4 +1,17 @@
+@echo off
+:: Add all changes to staging
 git add .
-git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
+
+:: Get the current date and time
+for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set curDate=%%a-%%b-%%c
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set curTime=%%a-%%b
+
+:: Commit with the date and time as the message
+git commit -m "%curDate% %curTime%"
+
+:: Push changes
 git push
-read -p "Press any key to close this window..."
+
+:: Pause before closing
+echo Press any key to close this window...
+pause > nul
