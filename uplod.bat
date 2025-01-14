3c47fa5435d4f74aa6f2fab893a1401364567abc
Alfa <210711378@students.uajy.ac.id>
2025-01-14 09:08:17 +0700

    Tue-01-14 09-08-%c

--- uplod.bat
+++ uplod.bat
@@ -4,7 +4,7 @@ git add .
 
 :: Get the current date and time
 for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set curDate=%%a-%%b-%%c
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set curTime=%%a-%%b
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set curTime=%%a-%%b-%%c
 
 :: Commit with the date and time as the message
 git commit -m "%curDate% %curTime%"
