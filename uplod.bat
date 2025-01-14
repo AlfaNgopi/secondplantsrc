7752d374fb529d4b74567de21563b243c8559e97
Alfa <210711378@students.uajy.ac.id>
2025-01-14 09:11:36 +0700

    Tue-01-14 09-11 #2

--- uplod.bat
+++ uplod.bat
@@ -4,10 +4,19 @@ git add .
 
 :: Get the current date and time
 for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set curDate=%%a-%%b-%%c
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set curTime=%%a-%%b-%%c
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set curTime=%%a-%%b
 
-:: Commit with the date and time as the message
-git commit -m "%curDate% %curTime%"
+:: Read the current value of iterator from iterator.txt
+set /p i=<iterator.txt
+
+:: Increment the iterator
+set /a i+=1
+
+:: Save the updated iterator back to iterator.txt
+echo %i% > iterator.txt
+
+:: Commit with the date, time, and iterator as the message
+git commit -m "%curDate% %curTime% #%i%"
 
 :: Push changes
 git push
